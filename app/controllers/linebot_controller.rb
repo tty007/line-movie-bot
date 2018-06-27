848f9fc5a6eb582e4f4d940784554a5625924dd6
tty007 <c7dc007@gmail.com>
2018-06-27 15:33:25 +0900

    fix2

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -26,7 +26,7 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-            if message["text"] == "あ"
+            if message == "あ"
                 messages = [
                     "ラブリーボーン(2009)/ドラマ",
                     "her(2013)/SF,ラブストーリー",
